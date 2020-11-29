2c2f2a0b11d548e9101eec0903529bb3f1c38d57
cy <eric@lorefolk.com>
2020-11-29 15:36:43 -0600

    final bundle shim process and Buffer

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
+const webpack = require('webpack');
 const path = require('path');
 module.exports = {
   entry: "./src/index.ts",
   externals: {
     'simple-peer': {
-      commonjs: 'simple-peer'
+      commonjs: 'simple-peer',
+      commonjs2: 'simple-peer'
     }
   },
   output: {
+    library: 'SimplePeerFiles',
+    libraryTarget: 'umd',
     filename: "simple-peer-files.js",
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
-    extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+    extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+    alias: {
+      process: "process/browser"
+    }
   },
   module: {
     rules: [{ test: /\.ts$/, loader: "ts-loader" }]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: 'buffer',
+      process: 'process/browser'
+    })
+  ]
 }
